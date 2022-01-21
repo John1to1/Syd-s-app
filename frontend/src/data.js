bb9e7990fa49d420ec099169a35eaf77e3cf0f35
John1to1 <giuseppe.quarata@live.it>
2022-01-21 15:49:23 -0700

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    product:[
+        {
+            _id:'1',
+            name: 'Pokemon Dragon',
+            category: 'Draw',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Syd',
+            rating:4.5,
+            numReviewa:10,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name: 'Pokemon 2',
+            category: 'Draw',
+            image:'/images/p2.jpg',
+            price:100,
+            brand:'Syd',
+            rating:4,
+            numReviewa:10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name: 'Pokemon 3',
+            category: 'Draw',
+            image:'/images/p3.jpg',
+            price:80,
+            brand:'Syd',
+            rating:5,
+            numReviewa:70,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name: 'Pokemon 4',
+            category: 'Draw',
+            image:'/images/p4.jpg',
+            price:145,
+            brand:'Syd',
+            rating:5,
+            numReviewa:109,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name: 'Pokemon 5',
+            category: 'Draw',
+            image:'/images/p5.jpg',
+            price:56,
+            brand:'Syd',
+            rating:3,
+            numReviewa:45,
+            description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name: 'Pokemon 6',
+            category: 'Draw',
+            image:'/images/p6.jpg',
+            price:120,
+            brand:'Syd',
+            rating:4.5,
+            numReviewa:10,
+            description: 'high quality product'
+        },
+    ],
+};
+export default data;
